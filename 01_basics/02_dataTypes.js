e1eed8e4b5c05d048583046af93c309a1c677e96
Rutuja-Pandhare <pandharerutuja25@gmail.com>
2025-12-08 08:00:12 +0000

    variables and dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 +3) //we are using node.js, not browser sp syntax of writting alert is different
+console.log(3 
+    +
+    3); // code readability should be high
+
+console.log("Rutuja")
+
+let name = "rutuja"
+let age = 22
+let isLogggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined yet
+// symbol => find uniqueness
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+
+console.log(null);//object
+
+
+
