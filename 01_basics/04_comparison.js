e6b4e2b3db80fd342e9985894c11c1f76c908f7e
Rutuja-Pandhare <pandharerutuja25@gmail.com>
2025-12-16 07:22:50 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//never use this methods they can put u in confusion.
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//=== -> use this it will strictly check the data type
+console.log("2" === 2);
+
+
+
+
