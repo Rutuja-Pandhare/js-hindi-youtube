e83d7017bd105b91d266f39ff90bd76834672ba7
Rutuja-Pandhare <pandharerutuja25@gmail.com>
2026-01-18 14:00:09 +0000

    for loops

---
+++ 05_Itrations/five.js
@@ -0,0 +1,37 @@
+const coding=['js','ruby','python','java','cpp']
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index,arr);
+// })
+
+const myCoding= [
+    {
+        languageName: "javaScript",
+        languageFileName:"java"
+    },{
+        languageName: "java",
+        languageFileName:"java"
+    },
+    {
+        languageName: "python",
+        languageFileName:"py"
+    },
+]
+myCoding.forEach((item) =>{
+    console.log(item.languageName);
+})
